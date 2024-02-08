d9985bbd914c4f47ac1b9321d2fc43161ec419e2
chandraketikapatel <159416029+chandraketikapatel@users.noreply.github.com>
2024-02-08 15:37:42 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title('IRIS FLOWER  DASHBOARD')
+st.divider()
+st.subheader("Descripation")
+st.write(' The flower is plant genus of 310 accepeted species with showy flowers.As well as being the scientific name,iris is also widely used as a common name iris species')
+col1,col2,col3=st.columns(3)
